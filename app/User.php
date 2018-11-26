c00efe77f26297793bfd44bd2c20bf7fc1a14603
Mauricio Bernert <mauriciobernert@Mac-mini-de-Mauricio.local>
2018-11-26 19:06:58 -0200

    migrations and models

--- app/User.php
+++ app/User.php
@@ -27,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function favorites() {
+        return $this->belongsToMany(Post::class, 'favorites', 'user_id', 'post_id');
+    }
 }
